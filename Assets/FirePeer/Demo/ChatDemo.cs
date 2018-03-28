d04c113d196fb6bc851f0dc86902b4b029574f45
adrenak <vatsalatfei@gmail.com>
2018-03-28 09:52:30 +0530

    Improve default WebRTC code

---
+++ Assets/FirePeer/Demo/ChatDemo.cs
@@ -0,0 +1,36 @@
+﻿using Byn.Net;
+using System.Text;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ChatDemo : MonoBehaviour {
+    AirPeer[] m_Peers = new AirPeer[4];
+    int index = -1;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            index++;
+            Starts();
+        }
+        if (Input.GetKeyDown(KeyCode.A)) 
+            m_Peers[2].DisconnectFromServer();
+    }
+
+    private void Starts() {
+        m_Peers[index] = AirPeer.CreateInstance((index).ToString());
+        m_Peers[index].OnNetworkEvent += HandlePeerOnNetworkEvent;
+
+        if(index == 0) {
+            if (m_Peers[index].StartNetwork())
+                m_Peers[index].StartServer("test-chat", _ => {});
+        }
+        else {
+            if (m_Peers[index].StartNetwork())
+                m_Peers[index].JoinServer("test-chat", _ => { });
+        }
+    }
+
+    private void HandlePeerOnNetworkEvent(AirPeer sender, NetworkEvent netEvent) {
+        Debug.Log("Message from " + netEvent.ConnectionId + " to " + sender.name + " => " + netEvent.GetMessageAsString());
+    }
+}
---
+++ Assets/FirePeer/Scripts/WebRTCExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using Byn.Net;
+using System.Text;
+
+public static class WebRTCExtensions {
+    public static string GetMessageAsString(this NetworkEvent netEvent) {
+        if (netEvent.MessageData == null) return netEvent.Type.ToString();
+        MessageDataBuffer buffer = netEvent.MessageData;
+        string msg = Encoding.UTF8.GetString(buffer.Buffer, 0, buffer.ContentLength);
+        buffer.Dispose();
+        return msg;
+    }
+}
